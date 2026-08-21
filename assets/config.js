66d430015566366a0026a0803b22e2b853e572cf
markaziapp <salmabadimohamad@gmail.com>
2026-08-21 07:31:37 +0330

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 // آدرس API خود را بعد از دیپلوی Worker این‌جا جایگزین کنید
 // مثال: https://tolid-markazi-api.YOUR-SUBDOMAIN.workers.dev
-window.API_BASE = "https://tolid-markazi-api.YOUR-SUBDOMAIN.workers.dev";
+window.API_BASE = "https://tolid-markazi.salmabadimohamad.workers.dev";
